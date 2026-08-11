98306dfe592bf0ee7c9fab6fb420ed3d56dbccdd
Goofy32 <tahrir2003@gmail.com>
2026-08-11 14:43:52 +0700

    chore: limit scheduler to batch report cleanup

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('model:prune')->daily();
-Schedule::command('cleanup:soft-deletes')->daily();
 Schedule::command('reports:cleanup-batch-docx')->daily();
-;
