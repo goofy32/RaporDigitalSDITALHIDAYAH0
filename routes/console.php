eb594f52efcf7ab2a6a63a82671488ec18fd2340
Goofy32 <tahrir2003@gmail.com>
2026-08-11 12:43:18 +0700

    chore: remove example inspire schedule

--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,7 @@
 <?php
-
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('model:prune')->daily();
 Schedule::command('cleanup:soft-deletes')->daily();
 Schedule::command('reports:cleanup-batch-docx')->daily();
-
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+;
